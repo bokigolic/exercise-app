cc8c94356ced9f62503f86115968457704e6d040
bokigolic <bokigolic33@gmail.com>
2025-09-27 23:05:33 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>ExerciseDB Gym App 💪</h1>
+      <p>Welcome! We will add exercises here soon.</p>
+    </div>
+  );
+}
+
+export default App;
